5407b08ae206a5d5672d2b36a9193104eae4d157
emarishkin <ee.marishkin@yandex.ru>
2025-04-30 17:50:46 +0400

    header-css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,18 +8,20 @@ interface HeaderProps{
 export const Header:FC<HeaderProps> = () => {
     return (
         <header className="header">
-            <div className="logo">
-                <img className="logo-img" src={logo} alt="logo-panda" />
-                <h3>Panda CN Express</h3>
-            </div>
-            <nav className="nav">
-                <a href="#tariffs">Тарифы</a>
-                <a href="#services">Услуги</a>
-                <a href="#steps">Этапы</a>
-                <a href="#reviews">Отзывы</a>
-                <a href="#contacts">Контакты</a>
-            </nav>
+            <div className="container header-inner">
+                <div className="logo">
+                    <img className="logo-img" src={logo} alt="logo-panda" />
+                    <h3>Panda CN Express</h3>
+                </div>
+                <nav className="nav">
+                    <a href="#tariffs">Тарифы</a>
+                    <a href="#services">Услуги</a>
+                    <a href="#steps">Этапы</a>
+                    <a href="#reviews">Отзывы</a>
+                    <a href="#contacts">Контакты</a>
+                </nav>
             <button className="cta-button">Оставить заявку</button>
+            </div>
         </header>
     )
 }
